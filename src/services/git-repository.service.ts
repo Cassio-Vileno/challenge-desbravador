4d282a0103e2619ea147a8fb27758ea399043fb2
Cassio-Vileno <cassiovileno@gmail.com>
2023-10-20 10:15:21 -0300

    refactor: corrção service

--- src/services/git-repository.service.ts
+++ src/services/git-repository.service.ts
@@ -2,7 +2,7 @@ import api from "./api.service";
 
 export default class RepositoryService {
   static async getRepositories(username: string) {
-    const { data } = await api.get(`users/${username}/repos?sort=desc&order=stars`);
+    const { data } = await api.get(`users/${username}`);
     return data;
   }
 }
